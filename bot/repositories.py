7f34078bc2a09d1c392e0d4a0c7ce1c2e0421ac6
Kristina Sharaeva <kristinasharaeva1999@gmail.com>
2023-04-30 19:59:25 +0400

    Basic analysis done

--- bot/repositories.py
+++ bot/repositories.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from bot.settings import DB_URI
-from models import Base
+from bot.models import Base
 
 engine = create_engine(DB_URI)
 Base.metadata.create_all(engine)
